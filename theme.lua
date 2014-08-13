7fa1a1328657f9e705ff4c72f98d997b269f75f5
Thomas Hartman <rokstar83@gmail.com>
2014-08-13 11:07:19 -0400

    Added icons and a number of other changes.

--- theme.lua
+++ theme.lua
@@ -16,16 +16,18 @@ sharedthemes 		= shared .. "/themes"
 themes 					= config .. "/themes"
 themename 			= "/bigbang"
 
+themeicons = config .. themename .. "/icons"
+
 -- 
 
-theme.font					= "proggy 10"
+theme.font					= "proggytiny 10"
 
-theme.bg_normal     = "#222222"
+theme.bg_normal     = "#003366"
 theme.bg_focus      = "#000044"
-theme.bg_urgent     = "#C0C0C0"
-theme.bg_minimize   = "#444444"
+theme.bg_urgent     = "#CE8F34"
+theme.bg_minimize   = "#6699cc"
 
-theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#E0E0E0"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
@@ -47,32 +49,65 @@ theme.menu_width  = "200"
 -- beautiful.variable in your rc.lua
 --theme.bg_widget = "#cc0000"
 
+titlebar = sharedthemes .. "/default/titlebar"
+
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = titlebar .. "/close_normal.png"
+theme.titlebar_close_button_focus               = titlebar .. "/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive     = titlebar .. "/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive      = titlebar .. "/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active       = titlebar .. "/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active        = titlebar .. "/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive    = titlebar .. "/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive     = titlebar .. "/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active      = titlebar .. "/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active       = titlebar .. "/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive  = titlebar .. "/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive   = titlebar .. "/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active    = titlebar .. "/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active     = titlebar .. "/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = titlebar .. "/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = titlebar .. "/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active   = titlebar .. "/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active    = titlebar .. "/maximized_focus_active.png"
+
+-- Widget icons
+
+-- Wifi icons
+
+theme.widget_wifi_hi = "/home/thartman/projects/bigbang/icons/wifi_hi.png"
+theme.widget_wifi_med = "/home/thartman/projects/bigbang/icons/wifi_med.png"
+theme.widget_wifi_low = "/home/thartman/projects/bigbang/icons/wifi_lo.png"
+theme.widget_wifi_no = "/home/thartman/projects/bigbang/icons/wifi_no.png"
+
+-- /Wifi icons
+
+-- Battery icons
+
+theme.widget_bat_full = "/home/thartman/projects/bigbang/icons/bat_full.png"
+theme.widget_bat_medfull = "/home/thartman/projects/bigbang/icons/bat_medfull.png"
+theme.widget_bat_half = "/home/thartman/projects/bigbang/icons/bat_half.png"
+theme.widget_bat_halfempty = "/home/thartman/projects/bigbang/icons/bat_halfempty.png"
+theme.widget_bat_low = "/home/thartman/projects/bigbang/icons/bat_low.png"
+theme.widget_bat_empty = "/home/thartman/projects/bigbang/icons/bat_empty.png"
+
+-- /Battery icons
+
+-- Speaker icons
+
+theme.widget_speaker_mute  = "/home/thartman/projects/bigbang/icons/speaker_mute.png"
+theme.widget_speaker_low  = "/home/thartman/projects/bigbang/icons/speaker_low.png"
+theme.widget_speaker_med  = "/home/thartman/projects/bigbang/icons/speaker_med.png"
+theme.widget_speaker_hi  = "/home/thartman/projects/bigbang/icons/speaker_hi.png"
+
+-- /Speaker icons
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
+theme.wallpaper_cmd = { "nitrogen --restore" }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
