68417c0122848a2ff0c63146c860df48c510fcba
Tom Hartman <thomas.lees.hartman@gmail.com>
2020-06-04 14:15:42 -0400

    Added layout tiles

--- theme.lua
+++ theme.lua
@@ -17,7 +17,7 @@ theme.widget_icon_dir = theme.icon_dir .. "/widgets"
 -- }}}
 
 --- Fonts & Colors -- {{{
-theme.font = "proggytiny 8"
+theme.font = "Sauce Code Pro Nerd Font Complete Mono 8"
 theme.bg_normal     = "#000000"
 theme.bg_focus      = "#000000"
 theme.bg_urgent     = "#CE8F34"
@@ -108,19 +108,20 @@ theme.titlebar_height                           = 14
 
 -- -- You can use your own command to set your wallpaper
 theme.wallpaper_cmd = { "nitrogen", "--restore" }
+
 -- -- You can use your own layout icons like this:
--- theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
--- theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
--- theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
--- theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
--- theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
--- theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
--- theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
--- theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
--- theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
--- theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
--- theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
--- theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
+theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
+theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
+theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
+theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
+theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
+theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
+theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
+theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
+theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
 
 --- Connection Icons -- {{{
 theme.connman_icon_dir = theme.icon_dir .. "/conn"
